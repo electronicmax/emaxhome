5520b8b30be65582d4291ee09e5240ec9f700e44
electronic Max <electronic@gmail.com>
2013-06-06 00:59:25 +0100

    boop

---
+++ data/all.js
@@ -0,0 +1,123 @@
+{
+   "chairs": [
+	   {
+		   "id":'pubchair-chi1',
+		   "title":"Proceedings Chair",
+           "conference":"CHI 2011",
+       },
+       {
+		   "id":"pubchair-chi2",
+		   "title":"Proceedings Chair",
+           "conference":"CHI 2012",
+       },
+       {
+		   "id":"datachair",
+		   "title":"Data Chair",
+           "conference":"CHI 2013",
+       },
+	   {
+		   "id":"cscw2014",
+		   "title":"Programme Committe (PC) Area Chair",
+           "conference":"CSCW 2014",
+       },
+	   {
+		   "id":"socm2013",
+		   "title":"Workshop Organiser"
+           "for":"SOCM 2013",
+           "conference":"WWW 2013",
+       },
+	   {
+		   "id":"temporal",
+		   "title":"Workshop Organiser",
+           "for":"Interacting with Temporal Data",
+           "conference":"CHI 2009",
+       }
+   ],
+   "publications": [
+	   {
+		   "id": "phdthesis",
+		   "title":"Attention, Memory, Effort and Time: Paths to More Effective Personal Information Management",
+		   "type":["Ph.D. Thesis"],
+		   "author":["Max Van Kleek"],
+		   "url":"http://dspace.mit.edu/handle/1721.1/66466",
+		   "year":2011,
+		   "topics":["personal information management", "list-it", "field studies"]
+	   },
+	   {
+		   "id": "mengthesis",
+		   "title":"Intelligent Environments for Informal Public Spaces: the Ki/o Kiosk Platform",
+		   "type":["M.Eng Thesis"],
+		   "url":"http://people.csail.mit.edu/emax/papers/kiothesis2003.pdf",
+		   "year":2003,
+		   "topics":["ubiquitous computing", "field study", "software architecture", "smart spaces"]
+	   },
+	   {
+		   "id": "twiage",
+		   "title":"Twiage: A Game for Finding Good Advice on Twitter",
+		   "authors":["Max Van Kleek", "Daniel A. Smith", "Ruben Stranders", "mc schraefel"],		   
+		   "type":["conference paper"],		   
+		   "url":"http://people.csail.mit.edu/emax/papers/chi2013-carpe-data.pdf",
+		   "doi":"http://dl.acm.org/citation.cfm?doid=2207676.2208530",
+		   "conference":"CHI 2012",
+		   "year":2012,
+		   "topics":["ubiquitous computing", "field study", "software architecture", "smart spaces"]
+	   },	   
+	   {
+		   "id": "carpe-data",
+		   "title":"Carpe Data: Supporting Serendipitous Data Integration in Personal Information Management",
+		   "authors":["Max Van Kleek", "Daniel A. Smith", "Heather Packer", "James Skinner", "Nigel Shadbolt"],
+		   "type":["conference paper"],		   
+		   "url":"http://people.csail.mit.edu/emax/papers/chi2013-carpe-data.pdf",
+		   "year":2013,
+		   "topics":["ubiquitous computing", "field study", "software architecture", "smart spaces"]
+	   },
+	   {
+		   "id":"infoscraps",
+		   "title":"Information scraps: How and why information eludes our personal information management tools",
+		   "authors":["Michael Bernstein", "Max Van Kleek", "David Karger", "mc schraefel"],
+		   "type":["journal paper"],
+		   "journal_name":"TOIS",
+		   "year":2008,
+		   "doi":"http://dl.acm.org/citation.cfm?doid=1402256.1402263",
+		   "url":"http://people.csail.mit.edu/emax/papers/TOIS-infoscraps-cameraready.pdf"
+	   },
+	   {
+		   "id":"pim2012-webbox",
+		   "title":"A decentralized architecture for consolidating personal information ecosystems: The WebBox",
+		   "authors":["Max Van Kleek", "Daniel A. Smith", "Nigel Shadbolt", "m.c. schraefel"],
+		   "type":["workshop paper"],
+		   "conference":"PIM 2012",
+		   "year":2012,
+		   "url":"http://users.ecs.soton.ac.uk/~mvk/webbox-pim.pdf"
+	   },	   
+	   {
+		   "id":"eyebrowse",
+		   "title":"Eyebrowse: real-time web activity sharing and visualization",
+		   "authors":["Max Van Kleek","Brennan Moore", "Christina Xu", "David Karger"],
+		   "type":["short paper"],
+		   "conference":"CHI 2010",
+		   "year":2010,
+		   "url":"http://people.csail.mit.edu/emax/papers/eyebrowse-wip299-vankleek.pdf"
+	   },
+	   {
+		   "id":"atomate",
+		   "title":"Atomate it! end-user context-sensitive automation using heterogeneous information sources on the web",
+		   "authors":["Max Van Kleek","Brennan Moore", "Paul Andre", "David Karger", "mc schraefel"],
+		   "type":["short paper"],
+		   "conference":"CHI 2010",
+		   "year":2010,
+		   "url":"http://people.csail.mit.edu/emax/papers/atomate-www2010-camera.pdf",
+		   "doi":"http://dl.acm.org/citation.cfm?doid=1772690.1772787"
+	   },	   
+	   {
+		   "id":"listit-chi2009",
+		   "title":"Note-to-Self: Examining Personal Information-Keeping in a Lightweight Note-Taking Tool",
+		   "authors":["Max Van Kleek","Michael Bernstein","Greg Vargas", "Katrina Panovich", "David Karger", "mc schraefel"],
+		   "type":["conference paper"],
+		   "conference":"CHI 2009",
+		   "year":2009,
+		   "url":"http://people.csail.mit.edu/emax/papers/listit-chi2009.pdf",
+		   "dpi":"http://dl.acm.org/citation.cfm?doid=1518701.1518924"
+	   }	   
+   ]
+}
