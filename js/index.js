ef8e130af8a8334b5d5c1b941baa48aa988fa5cc
electronic Max <electronic@gmail.com>
2013-06-06 21:00:42 +0100

    boop

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+
+angular
+	.module('emax', ['ui'])
+	.controller('TopLevel', function($scope, googleSheet, utils) {
+		var $sa = function(fn) { return utils.safe_apply($scope, fn); };
+		$scope.getTypes = function() {
+			var data = $scope.works || [];
+			return data.map(function(x) { return (x.type || '').split(','); });
+		};
+		$.getJSON('data/all.js').then(function(objs) {
+		 	$sa(function() {
+				_($scope).extend(objs);
+				console.log('contac t>> ', objs.contact);
+			});
+		}).fail(function(err) { console.error(err);});
+		
+	})
+	.directive('author', function() {
+		return {
+			restrict:'E',
+			replace:true,
+			scope:{val:'=data'},
+			template:"<div class='author'><span ng-bind='val'></div>"
+		};
+	})
+	.directive('contact', function() {
+		return {
+			restrict:'E',
+			scope:{val:'=data'},
+			template:"<a href='{{val.url}}' target='_new' ng-bind-unsafe='val.name'> {{ val.name }}</a>"
+		};
+	})
+	.directive('pub', function($compile) {
+		return {
+			restrict:'E',
+			scope:{val:'=data'},
+			template:"<div class='pub'><div class='title' ng-bind='val.title'></div><div class='authors'><div class='author' ng-repeat='a in val.authors'><author data='a'></author></div></div></div>"
+		};
+	});
+
+
+			// link: function (scope, element, attrs) {
+			// 	var template = 
+			// 	var newElement = angular.element(template);
+			// 	$compile(newElement)(scope);
+			// 	element.replaceWith(newElement);
+			// 	console.log('authors :: ', scope.val.authors);
+			// }
